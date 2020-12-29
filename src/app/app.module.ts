975f0370d244241b04931115ddbaa28f998c3f25
Gaurav patidar <gp26101998@gmail.com>
2020-12-29 16:57:54 +0530

    first tinker template design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { SayComponent } from './say/say.component';
+import { OurBlogPostsComponent } from './our-blog-posts/our-blog-posts.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    GallaryComponent,
+    SayComponent,
+    OurBlogPostsComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/our-blog-posts/our-blog-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-blog-posts',
+  templateUrl: './our-blog-posts.component.html',
+  styleUrls: ['./our-blog-posts.component.css']
+})
+export class OurBlogPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
